f0d4676ef0696e25437ba1a18ee9643b05d8d910
Don Aguinaldo <donald.aguinaldo@gmail.com>
2019-11-01 15:30:14 -0500

    Can connect to MySQL localhost.

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "Don",
+  password: "D23asm774#"
+});
+
+con.connect((err) => {
+  if(err) throw err;
+
+  console.log("Successfully connected to MySQL!");
+});
+
 app.get("/", (req, res) => {
-   res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => { console.log(`Server listening on port: ${port}.`)} );
